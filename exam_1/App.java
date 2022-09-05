584d24c6abe60f2340086fe9f0dd0080d0f5ddef
Emlan Implica <111472856+tteoguu@users.noreply.github.com>
2022-09-05 09:42:49 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) { 
+
+
+    Student Emlan = new Student();
+
+        Emlan.surname = "Implica";
+        Emlan.firstname = "Emlan";
+        Emlan.middleInitial = 'L';
+        Emlan.dateOfBirth = 03/20/2004;
+        Emlan.studentNumber = 2022102529;
+        Emlan.studentEmailAddress = "implicael@students.national-u.edu.ph";
+        Emlan.amIAwesome = true;
+        Emlan.sayMystudentNumber();
+        Emlan.sayMystudentEmailAddress();
+        Emlan.amIAwesome();
+
+
+    }
+}
